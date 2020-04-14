f0990834158b1a225cbe27ad6de3e9ca50f5daa8
decipher07 <social.deej@gmail.com>
2020-04-14 06:19:43 +0530

    Added UI

--- app/src/main/java/com/example/brainteaserapp/MainActivity.java
+++ app/src/main/java/com/example/brainteaserapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.brainteaserapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button goButton ;
+
+    public void start(View view){
+        goButton.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goButton = (Button) findViewById(R.id.goButton);
     }
 }
